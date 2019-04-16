5132bc3eb1d94b8b84d61c599f9664745d239f3b
Szymon Ratajczyk <furusenpai@gmail.com>
2019-04-16 13:17:46 +0200

    Projekt

---
+++ import.py
@@ -0,0 +1,32 @@
+import csv
+from os import listdir
+from os.path import isfile, join
+import sklearn.model_selection
+import pandas
+
+
+f = []
+
+car_path = 'dataset/car/'
+car = [f for f in listdir(car_path) if isfile(join(car_path, f))]
+noncar_path = 'dataset/noncar/'
+noncar = [f for f in listdir(noncar_path) if isfile(join(noncar_path, f))]
+
+
+for file in car:
+    f.append((car_path + file, 1))
+
+for file in noncar:
+    f.append((noncar_path + file, 0))
+
+with open('dataset.csv', 'wt', newline='') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow(('path', 'class'))
+    writer.writerows(f)
+
+dataframe = pandas.read_csv('dataset.csv')
+
+a, b = sklearn.model_selection.train_test_split(dataframe, random_state=50)
+
+a.to_csv('dataset/train.csv', index=False)
+b.to_csv('dataset/test.csv', index=False)
---
+++ train.py
@@ -0,0 +1,61 @@
+import numpy as np
+import tensorflow as tf
+import keras
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pandas
+
+
+train_df = pandas.read_csv('dataset/train.csv')
+test_df = pandas.read_csv('dataset/test.csv')
+
+train_paths = train_df['path']
+test_paths = test_df['path']
+
+train_class = train_df['class']
+test_class = test_df['class']
+
+car = []
+noncar = []
+
+for f in train_paths:
+    # print(f)
+    car.append(mpimg.imread(f))
+
+for f in test_paths:
+    # print(f)
+    noncar.append(mpimg.imread(f))
+
+class_names = ['car', 'noncar']
+
+# train_paths = train_paths / 255.0
+# test_paths = test_paths / 255.0
+
+print(car)
+print(noncar)
+
+print(train_class)
+print(test_class)
+
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_paths[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names)
+#
+# model = keras.Sequential()
+#
+# model.add(keras.layers.Flatten(input_shape=(28, 28)))
+# model.add(keras.layers.Dense(128, activation='relu'))
+# model.add(keras.layers.Dense(10, activation='softmax'))
+#
+# model.summary()
+#
+# model.compile(optimizer=keras.optimizers.Adam(0.001),
+#               loss='categorical_crossentropy', # ! sparse_categorical_crossentropy for without one-hot !
+#               metrics=['accuracy'])
+#
+# model.fit(train_paths, train_labels, epochs=5)
